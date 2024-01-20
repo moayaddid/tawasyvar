167d829c44ca50f42f425a348c6b0c1b36ba8771
MahmoudHaraba <mahmoud.h2001@hotmail.com>
2024-01-20 17:09:27 +0300

    sitemap after fixes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,25 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://tawasyme.com',
-    generateRobotsTxt: true, // (optional)
-    sitemapSize: 7000,
-    exclude : "https://tawasyme.com/admin/Sellers"
-    // ...other options
-  }
-  
+  siteUrl: process.env.SITE_URL || "https://tawasyme.com",
+  generateRobotsTxt: true, // (optional)
+  exclude: [
+    `/admin/*`,
+    `/seller/*`,
+    `/login`,
+    `/TradeMarksPolicy`,
+    `/TermsAndConditions`,
+    `/SubmitOrder`,
+    `/PrivacyPolicy`,
+    `/MyProfile`,
+    `/Orders`,
+    `/signup`,
+    `/verification`,
+    `/ar/404`,
+    `/404`,
+    `/ar/Orders`,
+    `/ar/MyProfile`,
+    `/ar/SubmitOrder`,
+    `/seller`,
+  ],
+  // ...other options
+};
