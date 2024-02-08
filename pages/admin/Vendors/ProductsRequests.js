88099d5bcc271e184f4df5426d3f8c06f6c0d138
MahmoudHaraba <mahmoud.h2001@hotmail.com>
2024-02-08 15:23:28 +0300

    fixed the admin/Vendors product request page

--- pages/admin/Vendors/ProductsRequests.js
+++ pages/admin/Vendors/ProductsRequests.js
@@ -6,8 +6,8 @@ import { useRouter } from "next/navigation";
 import { useQuery } from "react-query";
 
 function VendorsProductRequests() {
-    const router = useRouter();
-    const Api = createAxiosInstance(router);
+  const router = useRouter();
+  const Api = createAxiosInstance(router);
   const {
     data: productsRequests,
     isLoading,
@@ -18,7 +18,7 @@ function VendorsProductRequests() {
     refetchOnWindowFocus: false,
   });
 
-  async function fetchVendorsProductRequests () {
+  async function fetchVendorsProductRequests() {
     return await Api.get(`/api/admin/vendors-requests`);
   }
 
@@ -36,21 +36,29 @@ function VendorsProductRequests() {
           </button> */}
       </div>
       <hr className="pb-5" />
-     { isLoading == true ? <div className="w-full h-full flex justify-center items-center">
-        <TawasyLoader width = {400} height = {400}/>
-     </div> :
-      <div className="w-full h-full grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-2 gap-5">
-        {productsRequests &&
-          productsRequests.data.requests.length > 0 ?
-          productsRequests.data.requests.map((request , i) => {
-            return (
-              <AdminVendorProductRequest
-                key = {i}
-                request={request}
-                refetch={() => refetch()}
-              />
-            );
-          }) : <div className="text-lg text-center" >There are no Products Requests</div> }
+      {isLoading == true ? (
+        <div className="w-full h-full flex justify-center items-center">
+          <TawasyLoader width={400} height={400} />
+        </div>
+      ) : (
+        <div className="w-full h-full grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-2 gap-5">
+          {productsRequests &&
+          productsRequests.data.requests &&
+          productsRequests.data.requests.length > 0 ? (
+            productsRequests.data.requests.map((request, i) => {
+              return (
+                <AdminVendorProductRequest
+                  key={i}
+                  request={request}
+                  refetch={() => refetch()}
+                />
+              );
+            })
+          ) : (
+            <div className="text-lg text-center">
+              There are no Products Requests
+            </div>
+          )}
           {/* {data.map((request) => {
             return (
               <AdminVendorProductRequest
@@ -59,8 +67,8 @@ function VendorsProductRequests() {
               />
             );
           })} */}
-      </div>
-       }
+        </div>
+      )}
     </div>
   );
 }
