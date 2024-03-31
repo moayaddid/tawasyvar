4dd0f003d63af27afafd0a75964f9f15a2348765
MahmoudHaraba <mahmoud.h2001@hotmail.com>
2024-03-31 13:55:43 +0300

    attached products search

--- pages/admin/Products/AttachedProducts/index.js
+++ pages/admin/Products/AttachedProducts/index.js
@@ -5,7 +5,8 @@ import withLayoutAdmin from "@/components/UI/adminLayout";
 import TawasyLoader from "@/components/UI/tawasyLoader";
 import { Ring } from "@uiball/loaders";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { MdArrowForward, MdClose } from "react-icons/md";
 import { useQuery } from "react-query";
 
 const tableheading = [
@@ -27,6 +28,10 @@ function AttachedProducts() {
   const router = useRouter();
   const Api = createAxiosInstance(router);
   // const [selectedStore, setSelectedStore] = useState();
+  const [inSearch, setInSearch] = useState(false);
+  const [searching, setSearching] = useState(false);
+  const [searchedResults, setSearchedResults] = useState();
+  const searchRef = useRef();
   const [currentPage, setCurrentPage] = useState(1);
   const {
     data: attachedProducts,
@@ -48,6 +53,57 @@ function AttachedProducts() {
     document.querySelector(`#${id}`).scrollIntoView({ behavior: "smooth" });
   }
 
+  async function search(e) {
+    e.preventDefault();
+    setSearching(true);
+    try {
+      const response = await Api.get(
+        `api/admin/attached-search`,
+        {
+          params: { query: searchRef.current.value },
+        },
+        {
+          noSuccessToast: true,
+        }
+      );
+      const component = !response.data.products ? (
+        <div className="text-center text-black ">{response.data.message}</div>
+      ) : (
+        <table className="w-max min-w-full overflow-auto relative table-auto overflow-y-scroll ">
+          <thead className="sticky top-0 bg-white border-b-2 border-blue-500 overflow-y-scroll">
+            <tr className="text-sm font-semibold text-center border-b-2 border-blue-500 uppercase">
+              {tableheading.map((index) => (
+                <th className=" px-4 py-4 " key={index.heading}>
+                  {index.heading}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="text-lg font-normal text-gray-700 text-center h-[500px]">
+            {response?.data?.products &&
+              response?.data?.products?.map((prod) => {
+                return (
+                  <AdminAttachedProduct
+                    product={prod}
+                    key={prod.id}
+                    refetch={() => {
+                      refetch();
+                    }}
+                  />
+                );
+              })}
+          </tbody>
+        </table>
+      );
+      setSearchedResults(component);
+      setSearching(false);
+    } catch (error) {
+      setSearchedResults(<p className="text-center" >Something went wrong</p>);
+      setSearching(false);
+    }
+    setSearching(false);
+  }
+
   async function fetchAttachedProducts(selectedPage) {
     try {
       let storeId;
@@ -65,6 +121,55 @@ function AttachedProducts() {
         Attached Products
       </div>
       <div className="flex justify-start space-x-6 items-center">
+        <div
+          className=" w-[80%] flex justify-start items-center space-x-3 "
+          dir="ltr"
+        >
+          <form
+            dir={router.locale == "ar" ? "rtl" : "ltr"}
+            onSubmit={search}
+            className="flex bg-gray-100 w-[50%] items-center rounded-lg px-2 border-2 border-transparent focus-within:border-skin-primary transition-all duration-700 "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 mx-2"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+            <input
+              className="w-full bg-gray-100 outline-none rounded-lg text-sm h-10  "
+              type="text"
+              ref={searchRef}
+              // placeholder={`Search`}
+              placeholder={`Search By Name`}
+              onClick={() => {
+                setInSearch(true);
+              }}
+            />
+            <button type="submit">
+              <MdArrowForward
+                onClick={search}
+                className="hover:border-b-2 border-skin-primary cursor-pointer"
+              />
+            </button>
+          </form>
+          {inSearch == true && (
+            <MdClose
+              className="text-red-500 hover:text-red-600 w-[25px] h-[25px] hover:border-b-2 hover:border-red-600 cursor-pointer "
+              onClick={() => {
+                setInSearch(false);
+              }}
+            />
+          )}
+        </div>
         {attachedProducts && attachedProducts?.data?.pagination && (
           <div className="w-fit mx-auto flex justify-center items-center h-max gap-4 py-4 ">
             <button
@@ -87,7 +192,9 @@ function AttachedProducts() {
             >
               {`Previous Page`}
             </button>
-            <p className="border-b-2 border-skin-primary px-2 " >{attachedProducts.data.pagination.current_page}</p>
+            <p className="border-b-2 border-skin-primary px-2 ">
+              {attachedProducts.data.pagination.current_page}
+            </p>
             <button
               className="px-2 py-1 bg-gray-400 text-white rounded-lg hover:opacity-80 disabled:opacity-50 disabled:cursor-not-allowed w-max"
               onClick={() => {
@@ -107,9 +214,7 @@ function AttachedProducts() {
             <button
               className="px-2 py-1 bg-[#2837bf] text-white rounded-lg hover:opacity-80 disabled:opacity-50 disabled:cursor-not-allowed w-max"
               onClick={() => {
-                setCurrentPage(
-                  attachedProducts.data.pagination.last_page
-                );
+                setCurrentPage(attachedProducts.data.pagination.last_page);
               }}
               disabled={
                 attachedProducts.data.pagination.current_page ===
@@ -129,41 +234,52 @@ function AttachedProducts() {
           <TawasyLoader width={300} height={300} />
         </div>
       ) : (
-        <div className="w-full">
-          {attachedProducts && attachedProducts.data?.data?.length > 0 ? (
-            <table className="w-max min-w-full overflow-auto relative table-auto overflow-y-scroll ">
-              <thead className="sticky top-0 bg-white border-b-2 border-blue-500 overflow-y-scroll">
-                <tr className="text-sm font-semibold text-center border-b-2 border-blue-500 uppercase">
-                  {tableheading.map((index) => (
-                    <th className=" px-4 py-4 " key={index.heading}>
-                      {index.heading}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody className="text-lg font-normal text-gray-700 text-center h-[500px]">
-                {attachedProducts?.data?.data &&
-                  attachedProducts.data.data.map((prod, i) => {
-                    return (
-                      <AdminAttachedProduct
-                        product={prod}
-                        // selectedStoreId = {selectedStore}
-                        key={`${i} - ${prod.name} - ${prod.attached_id}`}
-                        refetch={() => {
-                          refetch();
-                        }}
-                      />
-                    );
-                  })}
-              </tbody>
-            </table>
-          ) : (
-            <p className="text-center text-lg">
-              There are no Attached Products
-            </p>
-          )}
-        </div>
+        inSearch == false && (
+          <div className="w-full">
+            {attachedProducts && attachedProducts.data?.data?.length > 0 ? (
+              <table className="w-max min-w-full overflow-auto relative table-auto overflow-y-scroll ">
+                <thead className="sticky top-0 bg-white border-b-2 border-blue-500 overflow-y-scroll">
+                  <tr className="text-sm font-semibold text-center border-b-2 border-blue-500 uppercase">
+                    {tableheading.map((index) => (
+                      <th className=" px-4 py-4 " key={index.heading}>
+                        {index.heading}
+                      </th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody className="text-lg font-normal text-gray-700 text-center h-[500px]">
+                  {attachedProducts?.data?.data &&
+                    attachedProducts.data.data.map((prod, i) => {
+                      return (
+                        <AdminAttachedProduct
+                          product={prod}
+                          // selectedStoreId = {selectedStore}
+                          key={`${i} - ${prod.name} - ${prod.attached_id}`}
+                          refetch={() => {
+                            refetch();
+                          }}
+                        />
+                      );
+                    })}
+                </tbody>
+              </table>
+            ) : (
+              <p className="text-center text-lg">
+                There are no Attached Products
+              </p>
+            )}
+          </div>
+        )
       )}
+
+      {inSearch == true &&
+        (searching == true ? (
+          <div className="w-full h-full">
+            <TawasyLoader width={300} height={300} />
+          </div>
+        ) : (
+          searchedResults && searchedResults
+        ))}
     </div>
   );
 }
