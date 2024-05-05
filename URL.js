b8690d7470aba568e22ce5df7d8c9972332eea07
MahmoudHaraba <mahmoud.h2001@hotmail.com>
2024-05-05 22:45:47 +0300

    url change to admin2

--- URL.js
+++ URL.js
@@ -2,6 +2,6 @@
 // const url = 'http://192.168.1.202:8000' ; 
 // const url = 'https://absi.tawasyme.com' ; 
 // const url = 'https://abbas2.tawasyme.com' ; 
-const url = 'https://admin1.tawasyme.com' ;
+const url = 'https://admin2.tawasyme.com' ;
     // const url = "https://flutter.deskindamas.com" ;
 export default url ;
