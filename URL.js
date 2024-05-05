2f7999667428f1ac858f0ca9b4370053a6a5622c
MahmoudHaraba <mahmoud.h2001@hotmail.com>
2024-05-05 13:57:29 +0300

    url fix for sellers

--- URL.js
+++ URL.js
@@ -1,7 +1,7 @@
 // const url = 'http://192.168.43.14:8000' ; 
-const url = 'http://192.168.1.202:8000' ; 
+// const url = 'http://192.168.1.202:8000' ; 
 // const url = 'https://absi.tawasyme.com' ; 
 // const url = 'https://abbas2.tawasyme.com' ; 
-// const url = 'https://admin.tawasyme.com' ;
+const url = 'https://admin1.tawasyme.com' ;
     // const url = "https://flutter.deskindamas.com" ;
 export default url ;
